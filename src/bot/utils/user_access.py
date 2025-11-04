f8f94449c4db1dc2b9a00b6f1ec56cee1ac7d44b
albertiq <alberttuygunov@gmail.com>
2025-11-04 11:02:22 +0500

    added check user access

--- src/bot/utils/user_access.py
+++ src/bot/utils/user_access.py
@@ -2,5 +2,4 @@ from settings import cfg
 
 
 async def check_user_access(username: str) -> bool:
-    """Проверяет, есть ли у пользователя доступ к настройкам"""
     return username in cfg.allowed_usernames
